8527578e3a075fbe3a634b98178e93c1bafdcbc8
ndoan05 <89599246+ndoan05@users.noreply.github.com>
2023-03-06 23:42:45 -0600

    Add files via upload

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import LoginComponent from "../components/loginPage.vue"
+
 import { createRouter, createWebHistory } from 'vue-router'
 
 // make all paths and names lowercase for consistency
@@ -6,8 +6,7 @@ const routes = [
   {
     path: '/login',
     name: 'login',  
-    props: true,
-    component: () => import('../components/loginPage.vue')
+    component: () => import('../views/Login.vue')
   },
   {
     path: '/',
@@ -19,40 +18,40 @@ const routes = [
     name: 'intakeform',
     props: true,
     component: () => import('../components/intakeForm.vue'),
-    meta: { requiresAuth: true, roles: ['editor']}
+    
   },
   {
     path: '/findclient',
     name: 'findclient',
     component: () => import('../components/findClient.vue'),
-    meta: { requiresAuth: true, roles: ['editor', 'viewer']}
+    
   },
   {
     path: '/updateclient/:id',
     name: 'updateclient',
     props: true,
     component: () => import('../components/updateClient.vue'),
-    meta: { requiresAuth: true, roles: ['editor']}
+    
   },
   {
     path: '/eventform',
     name: 'eventform',
     component: () => import('../components/eventForm.vue'),
-    meta: { requiresAuth: true, roles: ['editor']}
+    
   },
   {
     path: '/findevents',
     name: 'findevents',
     component: () => import('../components/findEvents.vue'),
-    meta: { requiresAuth: true, roles: ['editor', 'viewer']}
+    
   },
   {
     path: '/eventdetails/:id',
     name: 'eventdetails',
     props: true,
     component: () => import('../components/eventDetails.vue'),
-    meta: { requiresAuth: true, roles: ['editor']}
-  }
+  }  
+    
 ]
 const router = createRouter({
   history: createWebHistory(),
