fa2ee0ba905940c260fd62ef637bedf0344fabe2
TillDeEnd <98076185+TillDeEnd@users.noreply.github.com>
2023-03-07 20:02:55 -0600

    Updated service details.vue
    Services should be 90%. Need testing. Need dummy data to display. Will ask for a lookover and possibly help before making major changes. Will continue to look for small adjustments to make.

--- frontend/src/components/serviceDetails.vue
+++ frontend/src/components/serviceDetails.vue
@@ -2,7 +2,6 @@
 import useVuelidate from '@vuelidate/core'
 import { required } from '@vuelidate/validators'
 import axios from 'axios'
-import { DateTime } from 'luxon'
 const apiURL = import.meta.env.VITE_ROOT_API
 
 export default {
@@ -12,57 +11,25 @@ export default {
   },
   data() {
     return {
-      clientAttendees: [],
-      event: {
-        name: '',
-        services: [],
-        date: '',
-        address: {
-          line1: '',
-          line2: '',
-          city: '',
-          county: '',
-          zip: ''
-        },
-        description: '',
-        attendees: []
-      }
+      service: []
     }
   },
   created() {
-    axios.get(`${apiURL}/events/id/${this.$route.params.id}`).then((res) => {
-      this.event = res.data
-      this.event.date = this.formattedDate(this.event.date)
-      this.event.attendees.forEach((e) => {
-        axios.get(`${apiURL}/clients/id/${e}`).then((res) => {
-          this.clientAttendees.push(res.data)
-        })
-      })
+    axios.get(`${apiURL}/services/id/${this.$route.params.id}`).then((res) => {
+      this.service = res.data
     })
   },
   methods: {
-    // better formatted date, converts UTC to local time
-    formattedDate(datetimeDB) {
-      const dt = DateTime.fromISO(datetimeDB, {
-        zone: 'utc'
-      })
-      return dt
-        .setZone(DateTime.now().zoneName, { keepLocalTime: true })
-        .toISODate()
-    },
-    handleEventUpdate() {
-      axios.put(`${apiURL}/events/update/${this.id}`, this.event).then(() => {
+    handleServiceUpdate() {
+      axios.put(`${apiURL}/services/update/${this.id}`, this.service).then(() => {
         alert('Update has been saved.')
         this.$router.back()
       })
     },
-    editClient(clientID) {
-      this.$router.push({ name: 'updateclient', params: { id: clientID } })
-    },
-    eventDelete() {
-      axios.delete(`${apiURL}/events/${this.id}`).then(() => {
-        alert('Event has been deleted.')
-        this.$router.push({ name: 'findevents' })
+    serviceDelete() {
+      axios.delete(`${apiURL}/services/${this.id}`).then(() => {
+        alert('Service has been deleted.')
+        this.$router.push({ name: 'findservices' })
       })
     }
   },
@@ -71,7 +38,7 @@ export default {
     return {
       event: {
         name: { required },
-        date: { required }
+        status: { required }
       }
     }
   }
@@ -83,7 +50,7 @@ export default {
       <h1
         class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
       >
-        Update Event
+        Update Service
       </h1>
     </div>
     <div class="px-10 py-20">
@@ -91,21 +58,21 @@ export default {
         <div
           class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
         >
-          <h2 class="text-2xl font-bold">Event Details</h2>
+          <h2 class="text-2xl font-bold">Service Details</h2>
           <!-- form field -->
           <div class="flex flex-col">
             <label class="block">
-              <span class="text-gray-700">Event Name</span>
+              <span class="text-gray-700">Service Name</span>
               <span style="color: #ff0000">*</span>
               <input
                 type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                v-model="event.name"
+                v-model="service.name"
               />
-              <span class="text-black" v-if="v$.event.name.$error">
+              <span class="text-black" v-if="v$.service.name.$error">
                 <p
                   class="text-red-700"
-                  v-for="error of v$.event.name.$errors"
+                  v-for="error of v$.service.name.$errors"
                   :key="error.$uid"
                 >
                   {{ error.$message }}!
@@ -117,17 +84,17 @@ export default {
           <!-- form field -->
           <div class="flex flex-col">
             <label class="block">
-              <span class="text-gray-700">Date</span>
+              <span class="text-gray-700">Service Status</span>
               <span style="color: #ff0000">*</span>
               <input
-                type="date"
+                type="string"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                v-model="event.date"
+                v-model="service.status"
               />
-              <span class="text-black" v-if="v$.event.date.$error">
+              <span class="text-black" v-if="v$.service.status.$error">
                 <p
                   class="text-red-700"
-                  v-for="error of v$.event.date.$errors"
+                  v-for="error of v$.service.status.$errors"
                   :key="error.$uid"
                 >
                   {{ error.$message }}!
@@ -146,7 +113,7 @@ export default {
               <textarea
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 rows="2"
-                v-model="event.description"
+                v-model="service.description"
               ></textarea>
             </label>
           </div>
@@ -154,130 +121,7 @@ export default {
           <div></div>
           <div></div>
           <div></div>
-          <!-- form field -->
-          <div class="flex flex-col grid-cols-3">
-            <label>Services Offered at Event</label>
-            <div>
-              <label for="familySupport" class="inline-flex items-center">
-                <input
-                  type="checkbox"
-                  id="familySupport"
-                  value="Family Support"
-                  v-model="event.services"
-                  class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
-                  notchecked
-                />
-                <span class="ml-2">Family Support</span>
-              </label>
-            </div>
-            <div>
-              <label for="adultEducation" class="inline-flex items-center">
-                <input
-                  type="checkbox"
-                  id="adultEducation"
-                  value="Adult Education"
-                  v-model="event.services"
-                  class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
-                  notchecked
-                />
-                <span class="ml-2">Adult Education</span>
-              </label>
-            </div>
-            <div>
-              <label for="youthServices" class="inline-flex items-center">
-                <input
-                  type="checkbox"
-                  id="youthServices"
-                  value="Youth Services Program"
-                  v-model="event.services"
-                  class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
-                  notchecked
-                />
-                <span class="ml-2">Youth Services Program</span>
-              </label>
-            </div>
-            <div>
-              <label for="childhoodEducation" class="inline-flex items-center">
-                <input
-                  type="checkbox"
-                  id="childhoodEducation"
-                  value="Early Childhood Education"
-                  v-model="event.services"
-                  class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
-                  notchecked
-                />
-                <span class="ml-2">Early Childhood Education</span>
-              </label>
-            </div>
-          </div>
-        </div>
 
-        <!-- grid container -->
-        <div
-          class="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-        >
-          <h2 class="text-2xl font-bold">Address</h2>
-          <!-- form field -->
-          <div class="flex flex-col">
-            <label class="block">
-              <span class="text-gray-700">Address Line 1</span>
-              <input
-                type="text"
-                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                placeholder
-                v-model="event.address.line1"
-              />
-            </label>
-          </div>
-          <!-- form field -->
-          <div class="flex flex-col">
-            <label class="block">
-              <span class="text-gray-700">Address Line 2</span>
-              <input
-                type="text"
-                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                placeholder
-                v-model="event.address.line2"
-              />
-            </label>
-          </div>
-          <!-- form field -->
-          <div class="flex flex-col">
-            <label class="block">
-              <span class="text-gray-700">City</span>
-              <input
-                type="text"
-                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                placeholder
-                v-model="event.address.city"
-              />
-            </label>
-          </div>
-          <div></div>
-          <!-- form field -->
-          <div class="flex flex-col">
-            <label class="block">
-              <span class="text-gray-700">County</span>
-              <input
-                type="text"
-                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                placeholder
-                v-model="event.address.county"
-              />
-            </label>
-          </div>
-          <!-- form field -->
-          <div class="flex flex-col">
-            <label class="block">
-              <span class="text-gray-700">Zip Code</span>
-              <input
-                type="text"
-                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                placeholder
-                v-model="event.address.zip"
-              />
-            </label>
-          </div>
         </div>
 
         <!-- grid container -->
@@ -286,20 +130,20 @@ export default {
         >
           <div class="flex justify-between mt-10 mr-20">
             <button
-              @click="handleEventUpdate"
+              @click="handleServiceUpdate"
               type="submit"
               class="bg-green-700 text-white rounded"
             >
-              Update Event
+              Update Service
             </button>
           </div>
           <div class="flex justify-between mt-10 mr-20">
             <button
-              @click="eventDelete"
+              @click="serviceDelete"
               type="submit"
               class="bg-red-700 text-white rounded"
             >
-              Delete Event
+              Delete Service
             </button>
           </div>
           <div class="flex justify-between mt-10 mr-20">
@@ -315,41 +159,6 @@ export default {
 
         <hr class="mt-10 mb-10" />
 
-        <!-- grid container -->
-        <div
-          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-        >
-          <div>
-            <h2 class="text-2xl font-bold">List of Attendees</h2>
-            <h3 class="italic">Click table row to edit/display an entry</h3>
-          </div>
-          <div class="flex flex-col col-span-2">
-            <table class="min-w-full shadow-md rounded">
-              <thead class="bg-gray-50 text-xl">
-                <tr>
-                  <th class="p-4 text-left">Name</th>
-                  <th class="p-4 text-left">City</th>
-                  <th class="p-4 text-left">Phone Number</th>
-                </tr>
-              </thead>
-              <tbody class="divide-y divide-gray-300">
-                <tr
-                  @click="editClient(client._id)"
-                  v-for="client in clientAttendees"
-                  :key="client._id"
-                >
-                  <td class="p-2 text-left">
-                    {{ client.firstName + ' ' + client.lastName }}
-                  </td>
-                  <td class="p-2 text-left">{{ client.address.city }}</td>
-                  <td class="p-2 text-left">
-                    {{ client.phoneNumber.primary }}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
       </form>
     </div>
   </main>
