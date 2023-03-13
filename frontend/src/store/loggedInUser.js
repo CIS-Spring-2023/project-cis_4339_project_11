9c7eddb72590d3cb87651ded64eef08e055e68c7
ndoan05 <89599246+ndoan05@users.noreply.github.com>
2023-03-13 03:28:41 -0500

    Add files via upload

--- frontend/src/store/loggedInUser.js
+++ frontend/src/store/loggedInUser.js
@@ -9,6 +9,8 @@ export const useLoggedInUserStore = defineStore({
     return {
       name: "",
       isLoggedIn: false,
+      // isViewer: false,
+      // isEditor: false,
     }
   },
   // equivalent to methods in components, perfect to define business logic
