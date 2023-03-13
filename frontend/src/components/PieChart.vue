7fac416a8e5b99b5061076c8bce73eaf717a1e31
MatthewF802 <meflore8@cougarnet.uh.edu>
2023-03-13 10:30:07 -0500

    moved template to the top for testing on homepage, added a watcher and fixed the rendering issue on piechart

--- frontend/src/components/PieChart.vue
+++ frontend/src/components/PieChart.vue
@@ -1,10 +1,10 @@
 <script>
+//removed unneccessary code with advice from chat gpt (x and y values) pie charts do not need
+// done with Chatgpt help
+
 import { Chart, registerables } from 'chart.js'
 Chart.register(...registerables)
-//removed unneccessary code with advice from chat gpt (x and y values) pie charts do not need
 
-//Found in testing that pie chart does not always show up with current setup, shows up after making slight change
-//in code, even as minor as editing a comment, and saving
 export default {
   props: {
     label: {
@@ -15,42 +15,58 @@ export default {
     }
   },
   async mounted() {
-    const backgroundColor = this.chartData.map(() => this.getColor())
-    const borderColor = backgroundColor.map((e) =>
-      e.replace(/[\d\.]+\)$/g, '1)')
-    )
-    await new Chart(this.$refs.AttendanceChart, {
-      type: 'pie',
-      data: {
-        labels: this.label,
-        datasets: [
-          {
-            borderWidth: 1,
-            backgroundColor: backgroundColor,
-            borderColor: borderColor,
-            data: this.chartData
-          }
-        ]
-      },
-      options: {
-        plugins: {
-          legend: {
-            display: false
-          }
-        },
-        responsive: true,
-        maintainAspectRatio: true
-      }
-    })
+    await this.renderChart()
+  },
+  watch: {
+    // watches for any changes to the chart and updates it constantly so that it appears, coded with help from chatgpt
+    chartData() {
+      this.renderChart() //method the re render, coded with help from chatgpt
+    }
   },
   methods: {
     getColor() {
       let channel = () => Math.random() * 255
       return `rgba(${channel()}, ${channel()}, ${channel()}, 0.2)`
+    },
+    async renderChart() {
+      //updates the chart
+      if (this.chart) {
+        this.chart.destroy()
+      }
+      await this.$nextTick() // waits for data to change, re renders chart
+      const backgroundColor = this.chartData.map(() => this.getColor())
+      const borderColor = backgroundColor.map((e) =>
+        e.replace(/[\d\.]+\)$/g, '1)')
+      )
+      this.chart = new Chart(this.$refs.AttendanceChart, {
+        // new chart that replaces old one
+        type: 'pie',
+        data: {
+          labels: this.label,
+          datasets: [
+            {
+              borderWidth: 1,
+              backgroundColor: backgroundColor,
+              borderColor: borderColor,
+              data: this.chartData
+            }
+          ]
+        },
+        options: {
+          plugins: {
+            legend: {
+              display: false
+            }
+          },
+          responsive: true,
+          maintainAspectRatio: true
+        }
+      })
     }
   }
 }
 </script>
+
 <template>
   <div class="shadow-lg rounded-lg overflow-hidden">
     <canvas class="p-10" ref="AttendanceChart"></canvas>
