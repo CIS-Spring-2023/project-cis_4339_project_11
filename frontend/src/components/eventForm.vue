d86372ad7246261cfec39c199d836e67442fbd5b
TillDeEnd <98076185+TillDeEnd@users.noreply.github.com>
2023-04-29 17:55:33 -0500

    Done, hopefully.

--- frontend/src/components/eventForm.vue
+++ frontend/src/components/eventForm.vue
@@ -156,8 +156,9 @@ export default {
                   <label class="inline-flex items-center">
                     <input
                       type="checkbox"
-                      value="service.name"
-                      v-model="event.services[service.name]"
+                      :value="service.name"
+                      v-model="event.services"
+                      multiple
                       class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
                       notchecked
                     />
